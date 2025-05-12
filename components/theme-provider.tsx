d9636345961e5b5fbe1fe76b0692e1817910ab88
ymmxl-mbp <limmyyingran@gmail.com>
2025-05-13 00:16:31 +0800

    build fix

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -8,13 +8,13 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
 
-export function useTheme(){
-  const { theme, setTheme } = React.
-  useContext(
-    React.createContext({
-      theme: "dark",
-      setTheme: (theme: string) => {},
-    })
-  )
-  return { theme, setTheme };
-}
+// export function useTheme(){
+//   const { setTheme } = React.
+//   useContext(
+//     React.createContext({
+//       theme: "dark",
+//       setTheme: (theme: string) => {},
+//     })
+//   )
+//   return { setTheme };
+// }
